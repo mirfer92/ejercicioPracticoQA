c8f7a127edcb5ed22e91b521afd0a16241038fcd
Mirko Romay <mirfer.sis@gmail.com>
2024-03-05 12:57:07 -0400

    feat: first demo using cucumber, selenium, chai

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'module'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single']
+    }
+};
